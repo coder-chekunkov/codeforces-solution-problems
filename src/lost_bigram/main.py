78ad590ecf805f6959e0aca967c68b79b5a89ee4
coder-chekunkov@yandex.ru <coder-chekunkov@yandex.ru>
2021-12-17 14:50:36 +0300

    solution of new problem (python)

---
+++ src/lost_bigram/main.py
@@ -0,0 +1,16 @@
+#Level - 800
+#Link: https://codeforces.com/problemset/problem/1618/B
+
+for _ in range(int(input())):
+	n = int(input())
+	s = input().split()
+	for i in range(n - 3):
+		if s[i][1] != s[i + 1][0]:
+			s.insert(i + 1, s[i][1] + s[i + 1][0])
+			break
+	else:
+		s.append(s[-1][1] + 'a')
+	print(s[0][0], end="")
+	for i in range(n - 1):
+		print(s[i][1], end="")
+	print()
